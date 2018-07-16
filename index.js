6d41a2072a2575c8dc81610b0eecfd9964c078da
ngt kn <ngtkn@ngts-MacBook-Pro.local>
2018-07-16 15:17:56 -0700

    add AlbumList component

--- index.js
+++ index.js
@@ -2,12 +2,16 @@
 
 // Import lib to create component
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // create component
 const App = () => (
-    <Header headerText={'Albums!'} />
+    <View>
+        <Header headerText={'Albums!'} />
+        <AlbumList />
+    </View>
 );      
 
 // render it to device
