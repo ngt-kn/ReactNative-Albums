f18fd00a41f1b844a0f7d5275c15053afa284baa
ngt kn <ngtkn@ngts-MacBook-Pro.local>
2018-07-16 13:03:33 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+/** @format */
+
+// Import lib to create component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+// create component
+const App = () => (
+    <Header headerText={'Albums!'} />
+);      
+
+// render it to device
+AppRegistry.registerComponent('albums', () => App);
